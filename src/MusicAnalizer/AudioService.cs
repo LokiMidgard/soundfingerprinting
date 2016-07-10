49134a843cc32507fe5b9d9e7bc19dcf9fedad8e
Patrick Kranz <patrick-kranz@live.de>
2016-07-10 12:37:07 +0200

    added Sample

---
+++ src/MusicAnalizer/AudioService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using SoundFingerprinting.Audio;
+
+namespace MusicAnalizer
+{
+    internal class AudioService : IAudioService
+    {
+        public IReadOnlyCollection<string> SupportedFormats
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public AudioSamples ReadMonoSamplesFromFile(string pathToSourceFile, int sampleRate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AudioSamples ReadMonoSamplesFromFile(string pathToSourceFile, int sampleRate, int seconds, int startAt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
